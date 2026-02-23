85ae9aed17f1c549180be73591163d16d12c58f5
Mariem <158288845+Mariem112001@users.noreply.github.com>
2026-02-23 15:34:38 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,20 @@ terraform {
   }
 }
 
-resource "local_file" "test_file" {
-  content  = "test pour Module 1"
-  filename = "test.txt"
+# Crée un premier fichier
+resource "local_file" "test_file1" {
+  content  = "Plan de test Module 1 - fichier 1"
+  filename = "test1.txt"
+}
+
+# Crée un deuxième fichier
+resource "local_file" "test_file2" {
+  content  = "Plan de test Module 1 - fichier 2"
+  filename = "test2.txt"
+}
+
+# Crée un troisième fichier pour simuler un changement
+resource "local_file" "test_file3" {
+  content  = "Plan de test Module 1 - fichier 3 avec modification"
+  filename = "test3.txt"
 }
