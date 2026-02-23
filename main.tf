c2a70f01f043def9ce8b56402f247d8dbb28c790
Mariem <158288845+Mariem112001@users.noreply.github.com>
2026-02-23 15:25:52 +0100

    Refactor AWS provider configuration in main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
-  region                  = "us-east-1"
-  access_key              = "FAKE"
-  secret_key              = "FAKE"
+  region     = "us-east-1"
+  access_key = "FAKE"
+  secret_key = "FAKE"
 }
